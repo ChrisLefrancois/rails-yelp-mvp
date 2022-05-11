f56a9c09594183771f00c1cd5c6a5fac5f191d4e
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-05-11 12:22:09 -0400

    just need to add css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Restaurant.create({name: 'Joe beef', address: '5465 St-laurent', phone_number: '514-445-3635', category: 'chinese' })
+Restaurant.create({name: 'Joe beef', address: '5465 St-laurent', phone_number: '514-445-3635', category: 'chinese' })
